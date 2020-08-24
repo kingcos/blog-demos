01252ffa5d93f788595162dbbce1e0da6d8ee852
kingcos <2821836721v@gmail.com>
2020-08-24 19:43:32 +0800

    Add more comment

--- method-swizzle-demo/method-swizzle-demo/B/SubB.m
+++ method-swizzle-demo/method-swizzle-demo/B/SubB.m
@@ -16,32 +16,45 @@
         // 2⃣️ 交换 b & subB（b 来自父类，子类未实现，subB 来自子类，子类已实现）
         // 此时，子类对象使用正常，但父类对象调用时，若在子类方法中调回了当前方法将出现异常
         {
-            // 在子类获取未实现的继承方法时，将获取到父类上的方法：
+            // 在子类获取未实现的继承方法时，将获取到父类上的方法，即：
             // Method b1 = class_getInstanceMethod([B class], @selector(b));
             // b == b1
-            // --- BEFORE EXC MAP ---
+            // --- BEFORE EXC ---
             // SEL       IMP
             // B.b       B.b
-            // B.subB(unrecognized selector)
+            // B.subB    <NONE>
             // SubB.b    B.b
             // SubB.subB SubB.subB
-            // --- AFTER EXC MAP ---
+            // --- AFTER EXC ---
             // SEL       IMP
-            // B.b       SubB.subB
-            // B.subB(unrecognized selector)
-            // SubB.b    SubB.subB
-            // SubB.subB B.b
-            // 只进行交换时，B.b 将通过执行到 SubB.subB SEL 的 IMP；但在内部进行 [self subB] 时将直接无法找到 B.subB SEL。
+            // B.b       SubB.subB ★ 父类其实不应当被交换
+            // B.subB    <NONE>
+            // SubB.b    SubB.subB ★
+            // SubB.subB B.b       ★
+            // 只进行交换时，B.b 将通过执行到 SubB.subB SEL 的 IMP；但在内部进行 [self subB] 时将直接无法找到 B.subB SEL
             
             Method b = class_getInstanceMethod(self, @selector(b));
             Method subB = class_getInstanceMethod(self, @selector(subB));
             
+            // --- AFTER ADD ---
+            // SEL       IMP
+            // B.b       B.b
+            // B.subB    <NONE>
+            // SubB.b    SubB.subB ★
+            // SubB.subB SubB.subB
             BOOL isAddedMethodToSubClass = class_addMethod(self,
                                                            @selector(b),
                                                            method_getImplementation(subB),
                                                            method_getTypeEncoding(subB));
 
             if (isAddedMethodToSubClass) {
+                // --- AFTER REPLACE ---
+                // SEL       IMP
+                // B.b       B.b
+                // B.subB    <NONE>
+                // SubB.b    SubB.subB
+                // SubB.subB B.b       ★
+                
                 // 需要为子类添加父类的方法 b 为 subB 的实现，再替换 subB 为 b 的实现即可
                 class_replaceMethod(self,
                                     @selector(subB),
